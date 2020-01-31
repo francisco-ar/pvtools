25166f7d9c90b0c5871d71dbce82005f8897bf4a
Thomas Capelle <thomascapelle@gmail.com>
2020-01-31 09:03:04 +0100

    initial commit

---
+++ capetools/imports.py
@@ -0,0 +1,13 @@
+from fastcore.all import *
+
+import numpy as np
+import pandas as pd
+from pandas import DataFrame, Series
+
+import hdf5storage
+
+import matplotlib.pyplot as plt
+
+#typing
+from typing import Union, Callable, Optional
+Number = Union[float, int]
