92dc88822594a6e1b45da56b48e4e66bc2188ccc
Thomas Capelle <thomascapelle@gmail.com>
2020-02-05 09:42:54 +0100

    change pandas print options

--- capetools/imports.py
+++ capetools/imports.py
@@ -4,6 +4,10 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame, Series
 
+pd.set_option('display.max_rows', 999)
+pd.set_option('precision', 3)
+
+
 import hdf5storage
 
 import matplotlib.pyplot as plt
